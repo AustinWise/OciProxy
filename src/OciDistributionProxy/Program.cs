2b9d5bf9cc863bf2ffdb3f912fc634d713606a64
Austin Wise <AustinWise@gmail.com>
2023-03-25 22:02:59 -0700

    Basic shell of a program.

--- src/OciDistributionProxy/Program.cs
+++ src/OciDistributionProxy/Program.cs
@@ -1,3 +1,5 @@
+using OciDistributionProxy;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -8,27 +10,11 @@ var app = builder.Build();
 
 app.UseHttpsRedirection();
 
-var summaries = new[]
+app.MapGet("/", () =>
 {
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast = Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
+    return Results.Text(OciDistributionProxy.Properties.Resources.Index_html, "text/html");
 });
 
-app.Run();
+app.MapGroup("/v2/").MapRegsitryApi();
 
-internal record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
+app.Run();
---
+++ src/OciDistributionProxy/RegistryApi.cs
@@ -0,0 +1,18 @@
+﻿namespace OciDistributionProxy
+{
+    public static class RegistryApi
+    {
+        const string API_HEADER_KEY = "Docker-Distribution-API-Version";
+        const string API_HEADER_VALUE = "registry/2.0";
+
+        public static RouteGroupBuilder MapRegsitryApi(this RouteGroupBuilder group)
+        {
+            group.MapGet("/", (HttpContext http) =>
+            {
+                http.Response.Headers[API_HEADER_KEY] = API_HEADER_VALUE;
+                return "OK!";
+            });
+            return group;
+        }
+    }
+}
