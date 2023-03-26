61d29b9c089a668431464271f4939209c1bbaa3e
Austin Wise <AustinWise@gmail.com>
2023-03-26 09:07:06 -0700

    Only map GET method.

--- src/OciDistributionProxy/RegistryApi.cs
+++ src/OciDistributionProxy/RegistryApi.cs
@@ -7,12 +7,12 @@
 
         public static RouteGroupBuilder MapRegsitryApi(this RouteGroupBuilder group)
         {
-            group.MapMethods("/", new string[] { "GET", "HEAD" }, (HttpContext http) =>
+            group.MapGet("/", (HttpContext http) =>
             {
                 http.Response.Headers[API_HEADER_KEY] = API_HEADER_VALUE;
                 return Results.Ok();
             });
-            group.MapMethods("/{*remainder}", new string[] { "GET", "HEAD" }, (HttpContext http, string remainder) =>
+            group.MapGet("/{*remainder}", (HttpContext http, string remainder) =>
             {
                 http.Response.Headers[API_HEADER_KEY] = API_HEADER_VALUE;
                 return Results.Redirect($"https://us-central1-docker.pkg.dev/v2/oci-proxy/oci-proxy-images/{remainder}");
